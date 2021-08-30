49fad9ef2cc1111b7b8a4ad8f0d1eacca83c5c33
cosmin320-icl <79173861+cosmin320-icl@users.noreply.github.com>
2021-08-30 16:28:56 +0300

    So this is what I tried with parallel processing in the notebook

--- Functions/EmissionFunction0.py
+++ Functions/EmissionFunction0.py
@@ -13,35 +13,35 @@ photonsPerNano = 10
 nanoparticleFrequency = 10 # how many particles there are
 
 def CreateCoordinateArray(x, y, z):
-# returns test matrix
-dimensions = (y, x, z)
-array1 = np.zeros(dimensions, dtype = int)
-return array1
+    # returns test matrix
+    dimensions = (y, x, z)
+    array1 = np.zeros(dimensions, dtype = int)
+    return array1
 
 def CreateParticleArray(frequency, Matrix):
-# returns array of random voxel position in which nanoparticle == true. Used for testing
-array = []
-xArray = random.randint(0, np.shape(Matrix)[1], size = frequency)
-yArray = random.randint(0, np.shape(Matrix)[0], size = frequency)
-zArray = random.randint(0, np.shape(Matrix)[2], size = frequency)
-for i in range(frequency):
-randomCoordinate = (yArray[i], xArray[i], zArray[i])
-array.append(randomCoordinate)
-return array
+    # returns array of random voxel position in which nanoparticle == true. Used for testing
+    array = []
+    xArray = random.randint(0, np.shape(Matrix)[1], size = frequency)
+    yArray = random.randint(0, np.shape(Matrix)[0], size = frequency)
+    zArray = random.randint(0, np.shape(Matrix)[2], size = frequency)
+    for i in range(frequency):
+    randomCoordinate = (yArray[i], xArray[i], zArray[i])
+    array.append(randomCoordinate)
+    return array
 
 # initiate arrays
 Matrix = CreateCoordinateArray(x, y, z)
 nanoparticleArray = CreateParticleArray(nanoparticleFrequency, Matrix)
 
 def updateValues(Matrix, nanoparticleArray):
-for i in nanoparticleArray:
-# access voxel, then do...?
-Matrix[i[0]][i[1]][i[2]] = 1 # testing if access of matrix at positions are correct
-# create photon using PhotonClass(). photon objects stored in list?
-# for i in nanoparticleArray: // i is the position
-# for j in range(photonsPerNano)
-# photonArray.append(PhotonClass(i, weight??, direction?? ))) // weight and direction are beyond my understanding
-return Matrix
+    for i in nanoparticleArray:
+    # access voxel, then do...?
+    Matrix[i[0]][i[1]][i[2]] = 1 # testing if access of matrix at positions are correct
+    # create photon using PhotonClass(). photon objects stored in list?
+    # for i in nanoparticleArray: // i is the position
+    # for j in range(photonsPerNano)
+    # photonArray.append(PhotonClass(i, weight??, direction?? ))) // weight and direction are beyond my understanding
+    return Matrix
 
 
 print("Matrix before update")
