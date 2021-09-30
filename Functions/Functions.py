4b79dc827226267e8a85ceec6e5d9e346c856d79
cosmin320-icl <79173861+cosmin320-icl@users.noreply.github.com>
2021-09-30 17:27:48 +0300

    Minor fixes

--- Functions/Functions.py
+++ Functions/Functions.py
@@ -82,32 +82,6 @@ def lum_update (Voxel_Matrix , photon , coeff_absorb, coeff_scatter):
     #4th index is always 1, as you're always manipulating the second 3D matrix which corresponds to luminosity
     
     return
-'''
-### Material information (Dummy values) 
-n1=1.000293     #Refractive index (First medium)
-n2=1.333     #Refractive index (Second medium)
-theta1 = 0.524     #radian
-Pincident = 1     #Power(or energy?) of incident light
- 
-#Fresnel_equations
-Rs = ((n1*math.cos(theta1)-n2*math.sqrt(1-(n1/n2*math.sin(theta1))**2))/(n1*math.cos(theta1)+n2*math.sqrt(1-(n1/n2*math.sin(theta1))**2)))**2
-Rp = ((-n2*math.cos(theta1)+n1*math.sqrt(1-(n1/n2*math.sin(theta1))**2))/(n2*math.cos(theta1)+n1*math.sqrt(1-(n1/n2*math.sin(theta1))**2)))**2
-
- 
-
-Reff = 0.5*(Rs+Rp)     #Effective reflectance
-T = 1-Reff     #Transmittance
-PRef = Pincident*Reff     #Reflected Power = incident P * R
-PTra = Pincident*T     #Transmitted P = incident P * T
-print("Effective reflectance: {}, Transmittance: {}".format(Reff,T))
-print("Power after reflection: {}, P after Transmission: {}".format(PRef,PTra))
-
- 
-
-#New direction
-theta2 = math.asin(n1/n2*math.sin(theta1))
-print("New direction, theta2:", theta2)
-'''
 def euclid(x, y):
     #Euclid's algorithm. please ignore
     while(y):
@@ -133,7 +107,9 @@ def ReflectOrTransmit(n1, n2, theta1, Pincident):
     theta2 = math.asin(n1/n2*math.sin(theta1))
     
     return theta2, PRef, PTra
+    #Function impossible to complete
 def boundaryDetection(old_position, new_position,matrix,Pincident):
+    "detects boundaries"
     #assuming tags are inside the matrix
     #Problem: what if the boundary is in between two identical points?
     '''
@@ -151,21 +127,16 @@ def boundaryDetection(old_position, new_position,matrix,Pincident):
                     if(matrix[old_position.x,old_position.y,old_position.z]==matrix[old_position.x+k*factor+i,old_position.y+k*factor+j,old_position.z+k*factor+z]):
                         sem=true
         factor+=1
-    
     '''
     if(matrix[old_position.x,old_position.y,old_position.z]==matrix[new_position.x,new_position.y,new_position.tissue.z]):
         return new_position,0,Pincident
     else:
         rho=(new_position.x**2+new_position.y**2+new_position.z**2)**0.5
-        #toDo: calculate incident angle, use matrix to get n1, n2, and indicent power? 
-        #I'll just assume the incident angle is in radians.
+        #assumed the incident angle is in radians.
         phi=math.atan((((new_position.x-old_position.x)**2+(new_position.y-old_position.y)**2)**0.5)/(new_position.z-old_position.z))
         theta=math.atan((new_position.y-old_position.y)/(new_position.x-old_position.x))
-        #arrange call with Carlos and Sigurd, Get n1,n2, incident power from matrix.
-        #boundary angles
+       
         #also assumed phi is unchanged
-        #verify that no angles are actuall cos/sin
-        #double check coordinate systems
         theta2, PRefT, PTraT=ReflectOrTransmit(n1,n2,theta,Pincident)
         phi2, PRefP, PTraP=ReflectOrTransmit(n1,n2,phi,Pincident)
         #I am really not sure of how I am combining those two
@@ -178,6 +149,7 @@ def boundaryDetection(old_position, new_position,matrix,Pincident):
         #returning data
         return new_position, PRef, PTra
         '''
+#Never used
 def rouletteSurvive(photon, treshHold):
     """Determines if a photon survives or not based on its weight and a set reshold
     /aight Cosmin here: Please check the method I made in the Photon class. I think we should talk about it."""
@@ -269,13 +241,15 @@ def Determine_Weight(Matrix,photon,absoprtionCoeff,scatteringCoeff,Voxel_Matrix,
     return new_position, Pincident, LuminosityList,Matrix,Voxel_Matrix
 
 def Check_Weight(photon,treshold,tresholdSurvive):
+    
     if photon.weight<treshold:
         photon.rouletteSurvive(tresholdSurvive)
     return
+#Not done yet
 def checkOutside(position,matrixBoundaries):
     #check if position is outside, return true
     return False
-#Ahmed's stuff
+#Untested until the end
 def updateValues(Matrix, nanoparticleArray):
     for i in nanoparticleArray:
     # access voxel, then do...?
@@ -292,12 +266,42 @@ def CreateCoordinateArray(x, y, z):
     return array1
 
 def CreateParticleArray(frequency, Matrix):
+
     # returns array of random voxel position in which nanoparticle == true. Used for testing
     array = []
     xArray = random.randint(0, np.shape(Matrix)[1], size = frequency)
     yArray = random.randint(0, np.shape(Matrix)[0], size = frequency)
     zArray = random.randint(0, np.shape(Matrix)[2], size = frequency)
     for i in range(frequency):
-    randomCoordinate = (yArray[i], xArray[i], zArray[i])
+    randomCoordinate = [yArray[i], xArray[i], zArray[i]]
     array.append(randomCoordinate)
     return array
+
+
+#Testing Code:
+'''
+### Material information (Dummy values) 
+n1=1.000293     #Refractive index (First medium)
+n2=1.333     #Refractive index (Second medium)
+theta1 = 0.524     #radian
+Pincident = 1     #Power(or energy?) of incident light
+ 
+#Fresnel_equations
+Rs = ((n1*math.cos(theta1)-n2*math.sqrt(1-(n1/n2*math.sin(theta1))**2))/(n1*math.cos(theta1)+n2*math.sqrt(1-(n1/n2*math.sin(theta1))**2)))**2
+Rp = ((-n2*math.cos(theta1)+n1*math.sqrt(1-(n1/n2*math.sin(theta1))**2))/(n2*math.cos(theta1)+n1*math.sqrt(1-(n1/n2*math.sin(theta1))**2)))**2
+
+ 
+
+Reff = 0.5*(Rs+Rp)     #Effective reflectance
+T = 1-Reff     #Transmittance
+PRef = Pincident*Reff     #Reflected Power = incident P * R
+PTra = Pincident*T     #Transmitted P = incident P * T
+print("Effective reflectance: {}, Transmittance: {}".format(Reff,T))
+print("Power after reflection: {}, P after Transmission: {}".format(PRef,PTra))
+
+ 
+
+#New direction
+theta2 = math.asin(n1/n2*math.sin(theta1))
+print("New direction, theta2:", theta2)
+'''
